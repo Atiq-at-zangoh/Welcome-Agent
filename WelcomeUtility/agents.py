7cb0dbf8631dc922a6c40ef2fa5e41df209b7ac2
atiq-at-zangoh <atiq@zangoh.com>
2024-10-10 12:46:11 +0530

    Agents defined

--- WelcomeUtility/agents.py
+++ WelcomeUtility/agents.py
@@ -1,14 +1,21 @@
 from autogen import ConversableAgent
-from langchain_google_genai import ChatGoogleGenerativeAI
+from config import llm
 
 
 data_entry = ConversableAgent(name="DataEntry",
                               system_message="Your name is DataEntry and your task is to enter the data in excel sheet",
-                              human_input_mode="NEVER",
+                              llm_config=llm,
+                              human_input_mode="NEVER"
                               )
 
-email_composer = ConversableAgent()
+email_composer = ConversableAgent(name="Email_composer",
+                                  system_message="Your name is Email_composer and your task is to compose a welcome email for visitors",
+                                  llm_config=llm,
+                                  human_input_mode="NEVER")
 
-sender = ConversableAgent()
+sender = ConversableAgent(name="Sender",
+                              system_message="Your name is Sender and your task is to send email to visitor's email id",
+                              llm_config=llm,
+                              human_input_mode="NEVER")
 
 linkedin_auto = ConversableAgent()
