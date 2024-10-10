21efbefbe3cfd9e35192ff345412ed75ca1afb01
atiq-at-zangoh <atiq@zangoh.com>
2024-10-10 16:16:41 +0530

    error

--- WelcomeUtility/agents.py
+++ WelcomeUtility/agents.py
@@ -1,14 +1,27 @@
-from autogen import AssistantAgent
+import autogen
+from autogen import AssistantAgent, ConversableAgent
 from config import llm
 
-email_composer = AssistantAgent(name="Email_composer",
-                                  system_message="Your are an Email_composer and your task is to compose a welcome email for new visitor",
-                                  llm_config={"model": llm},
-                                  human_input_mode="NEVER")
+# email_composer = AssistantAgent(name="Email_composer",
+#                                   system_message="Your are an Email_composer and your task is to compose a welcome email for new visitor",
+#                                   llm_config={"model": llm},
+#                                   human_input_mode="NEVER")
 
-sender = AssistantAgent(name="Sender",
-                              system_message="Your name is Sender and your task is to send email on visitor's email id",
-                              llm_config=llm,
-                              human_input_mode="NEVER")
+# sender = AssistantAgent(name="Sender",
+#                               system_message="Your name is Sender and your task is to send email on visitor's email id",
+#                               llm_config=llm,
+#                               human_input_mode="NEVER")
 
-linkedin_auto = AssistantAgent()
+# linkedin_auto = AssistantAgent()
+
+
+config_list_gemini = autogen.config_list_from_json("model_config.json")
+
+agent = ConversableAgent(
+    name="My Agent",
+    llm_config = {"config_list" : config_list_gemini},
+    code_execution_config = False,
+    human_input_mode = "NEVER",
+    function_map = None
+)
+
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from WelcomeUtility.agents import email_composer
+from WelcomeUtility.agents import  agent
 
-task="Write a welcome email for the visitor, an event is scheduled at Zangoh. So email should be in 150 words"
-email_body=email_composer.generate_reply(messages=[{"content": task, "role": "user"}])
+# task="Write a welcome email for the visitor, an event is scheduled at Zangoh. So email should be in 150 words"
+# email_body=email_composer.generate_reply(messages=[{"content": task, "role": "user"}])
 
-print(email_body)
+# print(email_body)
+
+reply = agent.generate_reply(messages=[{"content": "Who are you?", "role": "user"}])
+print(reply['content'])
